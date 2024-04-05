dbcdcc8b4b965fa830e98e48f5c2591bada9bcd4
Max <m.kostarev2004@gmail.com>
2024-04-05 17:08:59 +0500

    fix: consts file added

---
+++ src/consts.ts
@@ -0,0 +1,97 @@
+export const params = [
+    {
+        id: 1,
+        name: "Параметр 1",
+        type: 'string'
+    },
+    {
+        id: 2,
+        name: "Параметр 2",
+        type: 'string'
+    },
+    {
+        id: 3,
+        name: "Параметр 3",
+        type: 'string'
+    },
+    {
+        id: 4,
+        name: "Параметр 4",
+        type: 'string'
+    },
+    {
+        id: 5,
+        name: "Параметр 5",
+        type: 'string'
+    },
+    {
+        id: 6,
+        name: "Параметр 6",
+        type: 'string'
+    },{
+        id: 7,
+        name: "Параметр 7",
+        type: 'string'
+    },
+    {
+        id: 8,
+        name: "Параметр 8",
+        type: 'string'
+    },
+    {
+        id: 9,
+        name: "Параметр 9",
+        type: 'string'
+    },
+    {
+        id: 10,
+        name: "Параметр 10",
+        type: 'string'
+    },
+
+]
+
+export const model = {
+    paramValues: [
+        {
+            paramId: 1,
+            value: "Значение 1"
+        },
+        {
+            paramId: 2,
+            value: "Значение 2"
+        },
+        {
+            paramId: 3,
+            value: "Значение 3"
+        },
+        {
+            paramId: 4,
+            value: "Значение 4"
+        },
+        {
+            paramId: 5,
+            value: "Значение 5"
+        },
+        {
+            paramId: 6,
+            value: "Значение 6"
+        },
+        {
+            paramId: 7,
+            value: "Значение 7"
+        },
+        {
+            paramId: 8,
+            value: "Значение 8"
+        },
+        {
+            paramId: 9,
+            value: "Значение 9"
+        },
+        {
+            paramId: 10,
+            value: "Значение 10"
+        },
+    ]
+}
